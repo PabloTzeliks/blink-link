ded5e902c8343e55920f56fb8cf377f3c62e9f34
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-13 23:06:10 -0300

    chore: change the application archive to a YAML type

---
+++ src/test/java/pablo/tzeliks/blink_link/BlinkLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package pablo.tzeliks.blink_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
