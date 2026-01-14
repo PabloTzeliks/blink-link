8518fb27dde580d5821875bf9ef465425176b6ee
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-14 00:00:37 -0300

    feat: add initial version of the project, the core are implemented, and in memory the app can short URLs and access them

---
+++ src/main/java/pablo/tzeliks/blink_link/service/EncoderService.java
@@ -0,0 +1,43 @@
+package pablo.tzeliks.blink_link.service;
+
+import org.springframework.stereotype.Service;
+import pablo.tzeliks.blink_link.logic.BaseEncoder;
+
+import java.math.BigInteger;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class EncoderService {
+
+    private final ConcurrentHashMap urlList = new ConcurrentHashMap<Long, String>();
+    private final AtomicLong IdGenerator = new AtomicLong(1000);
+
+    private BaseEncoder encoder;
+
+    public EncoderService(BaseEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    public String encode(String urlInput) {
+
+        long id = IdGenerator.incrementAndGet();
+
+        urlList.put(id, urlInput);
+
+        return encoder.encode(id);
+    }
+
+    public String decode(String urlInput) {
+
+        long decodedId = encoder.decode(urlInput);
+
+        System.out.println("decoded id: " + decodedId);
+
+        String url = (String) urlList.get(decodedId);
+
+        System.out.println("url decoded: " + url);
+
+        return url;
+    }
+}
