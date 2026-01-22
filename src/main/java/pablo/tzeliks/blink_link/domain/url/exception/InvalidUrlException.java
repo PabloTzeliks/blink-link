7233a4965a0baa96fe6a15be55411a12448dfc63
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-22 01:00:26 -0300

    refactor: add new Model for the URL Domain

---
+++ src/main/java/pablo/tzeliks/blink_link/domain/url/exception/InvalidUrlException.java
@@ -0,0 +1,7 @@
+package pablo.tzeliks.blink_link.domain.url.exception;
+
+public class InvalidUrlException extends RuntimeException {
+    public InvalidUrlException(String message) {
+        super(message);
+    }
+}
