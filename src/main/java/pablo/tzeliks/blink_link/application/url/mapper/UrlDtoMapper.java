90004ae6413c483cdd99d30fd3bcfc578993b98d
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-22 16:02:42 -0300

    feat: implement Domain URL mapper

--- src/main/java/pablo/tzeliks/blink_link/application/url/mapper/UrlDtoMapper.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/mapper/UrlDtoMapper.java
@@ -1,4 +1,41 @@
 package pablo.tzeliks.blink_link.application.url.mapper;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import pablo.tzeliks.blink_link.application.url.dto.CreateUrlRequest;
+import pablo.tzeliks.blink_link.application.url.dto.UrlResponse;
+import pablo.tzeliks.blink_link.domain.url.model.Url;
+
+import java.time.LocalDateTime;
+
+@Component
 public class UrlDtoMapper {
+
+    @Value("${app.base-url:https://blink.link}")
+    private String baseUrl;
+
+    public UrlResponse toDto(Url domain) {
+
+        String shortUrl = baseUrl.endsWith("/")
+                ? baseUrl + domain.getShortCode()
+                : baseUrl + "/" + domain.getShortCode();
+
+        return new UrlResponse(
+                domain.getOriginalUrl(),
+                shortUrl,
+                domain.getCreatedAt()
+        );
+
+
+    }
+
+    public Url toDomain(CreateUrlRequest request, Long id, String shortCode) {
+
+        return new Url(
+                id,
+                request.originalUrl(),
+                shortCode,
+                LocalDateTime.now()
+        );
+    }
 }
