1f34176615debc2dbbda6745c65988aef4cbddeb
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-22 17:35:45 -0300

    feat: implement URL Resolver Use Case

---
+++ src/main/java/pablo/tzeliks/blink_link/domain/url/exception/UrlNotFoundException.java
@@ -0,0 +1,7 @@
+package pablo.tzeliks.blink_link.domain.url.exception;
+
+public class UrlNotFoundException extends BusinessEsception {
+    public UrlNotFoundException(String message) {
+        super(message);
+    }
+}
