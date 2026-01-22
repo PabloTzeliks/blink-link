fe68e4a0c9a651d604361333e2563e36c95179f1
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-22 16:50:40 -0300

    feat: implement infraestructure Mapper to Persistence

--- src/main/java/pablo/tzeliks/blink_link/infraestructure/persistence/mapper/UrlEntityMapper.java
+++ src/main/java/pablo/tzeliks/blink_link/infraestructure/persistence/mapper/UrlEntityMapper.java
@@ -1,4 +1,29 @@
 package pablo.tzeliks.blink_link.infraestructure.persistence.mapper;
 
+import org.springframework.stereotype.Component;
+import pablo.tzeliks.blink_link.domain.url.model.Url;
+import pablo.tzeliks.blink_link.infraestructure.persistence.entity.UrlEntity;
+
+@Component
 public class UrlEntityMapper {
+
+    public UrlEntity toEntity(Url domain) {
+
+        return new UrlEntity(
+                domain.getId(),
+                domain.getOriginalUrl(),
+                domain.getShortCode(),
+                domain.getCreatedAt()
+        );
+    }
+
+    public Url toDomain(UrlEntity entity) {
+
+        return new Url(
+                entity.getId(),
+                entity.getOriginalUrl(),
+                entity.getShortCode(),
+                entity.getCreatedAt()
+        );
+    }
 }
