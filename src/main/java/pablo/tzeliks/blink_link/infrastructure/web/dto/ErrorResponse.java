7cdc0cca14e212e8369f0ba68c3d2c93c6640310
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-24 16:43:16 -0300

    refactor: implement the fields of 'ErrorResponse' Error DTO

--- src/main/java/pablo/tzeliks/blink_link/infrastructure/web/dto/ErrorResponse.java
+++ src/main/java/pablo/tzeliks/blink_link/infrastructure/web/dto/ErrorResponse.java
@@ -1,6 +1,18 @@
 package pablo.tzeliks.blink_link.infrastructure.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
 
-
+        String type,
+        String title,
+        int status,
+        String detail,
+        String instance,
+        LocalDateTime timestamp,
+        List<ValidationError> errors
 ) { }
---
+++ src/main/java/pablo/tzeliks/blink_link/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.blink_link.infrastructure.web.exception;
+
+public class GlobalExceptionHandler {
+}
