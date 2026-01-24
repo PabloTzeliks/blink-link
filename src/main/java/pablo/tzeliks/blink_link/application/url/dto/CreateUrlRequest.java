800595aa0d32da4e4d1f43f1bb3450ff0c85da13
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-24 16:36:17 -0300

    feat: add validation fields on the entry DTOs

--- src/main/java/pablo/tzeliks/blink_link/application/url/dto/CreateUrlRequest.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/dto/CreateUrlRequest.java
@@ -1,6 +1,8 @@
 package pablo.tzeliks.blink_link.application.url.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 /**
  * TODO : new Docs
@@ -11,5 +13,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @since 1.0.0
  */
 public record CreateUrlRequest(
-        @JsonProperty("original_url") String originalUrl
+
+        @JsonProperty("original_url")
+        @NotBlank(message = "Original URL must not be blank")
+        @URL(message = "Original URL must be a valid URL")
+        String originalUrl
 ) { }
--- src/main/java/pablo/tzeliks/blink_link/application/url/dto/ResolveUrlRequest.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/dto/ResolveUrlRequest.java
@@ -1,7 +1,13 @@
 package pablo.tzeliks.blink_link.application.url.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public record ResolveUrlRequest(
-        @JsonProperty("short_url") String shortUrl
+
+        @JsonProperty("short_url")
+        @NotBlank(message = "Short URL must not be blank")
+        @URL(message = "Short URL must be a valid URL")
+        String shortUrl
 ) { }
