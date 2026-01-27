a657a9e9e56296f6af8ddc89eefadf68bb519fde
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-26 23:57:32 -0300

    feat: implement the sad path Tests to the 'ResolveUrlUseCase' Class

--- src/test/java/pablo/tzeliks/blink_link/application/url/usecase/ResolveUrlUseCaseTest.java
+++ src/test/java/pablo/tzeliks/blink_link/application/url/usecase/ResolveUrlUseCaseTest.java
@@ -9,16 +9,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import pablo.tzeliks.blink_link.application.url.dto.ResolveUrlRequest;
 import pablo.tzeliks.blink_link.application.url.dto.UrlResponse;
 import pablo.tzeliks.blink_link.application.url.mapper.UrlDtoMapper;
+import pablo.tzeliks.blink_link.domain.url.exception.InvalidUrlException;
+import pablo.tzeliks.blink_link.domain.url.exception.UrlNotFoundException;
 import pablo.tzeliks.blink_link.domain.url.model.Url;
 import pablo.tzeliks.blink_link.domain.url.ports.UrlRepositoryPort;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class ResolveUrlUseCaseTest {
@@ -61,5 +62,41 @@ class ResolveUrlUseCaseTest {
 
         verify(repository).findByShortCode(shortCode);
         verify(mapper).toDto(urlFound);
+    }
+
+    @Test
+    @DisplayName("Should throw InvalidUrlException when request is empty")
+    void shouldThrowExceptionWhenRequestEmpty() {
+        // Arrange
+        String blankCode = "";
+        ResolveUrlRequest request = new ResolveUrlRequest(blankCode);
+
+        // Act & Assert
+        assertThrows(InvalidUrlException.class,
+                () -> useCase.execute(request));
+
+        // Verify
+        verifyNoInteractions(repository);
+        verifyNoInteractions(mapper);
+    }
+
+    @Test
+    @DisplayName("Should throw UrlNotFoundException when URL not found for the provided short code")
+    void shouldThrowExceptionWhenUrlNotFound() {
+        // Arrange
+        String nonExistentCode = "ghost";
+        ResolveUrlRequest request = new ResolveUrlRequest(nonExistentCode);
+
+        // 1. Repository returns empty when searching for non-existent short code
+        when(repository.findByShortCode(nonExistentCode)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(UrlNotFoundException.class,
+                () -> useCase.execute(request));
+
+        // Verify
+        verify(repository).findByShortCode(nonExistentCode);
+
+        verifyNoInteractions(mapper);
     }
 }
