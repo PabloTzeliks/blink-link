4cb6255cd7c46000632081ed8d39d2981e7d370a
PabloTzeliks <arq.pabloo@gmail.com>
2026-01-27 14:56:03 -0300

    feat: implement Abstract Container creator Class for the Integration Tests

--- src/test/java/pablo/tzeliks/blink_link/infrastructure/AbstractContainerBase.java
+++ src/test/java/pablo/tzeliks/blink_link/infrastructure/AbstractContainerBase.java
@@ -1,7 +1,29 @@
 package pablo.tzeliks.blink_link.infrastructure;
 
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-public class AbstractContainerBase {
+public abstract class AbstractContainerBase {
+
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+
+        // URL
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+
+        // USERNAME
+        registry.add("spring.datasource.username", postgres::getUsername);
+
+        // PASSWORD
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 }
