0fe9970bf47c2b4186914318047a24b9a6c3dc07
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-17 18:58:13 -0300

    feat: implement 'Plan' Enum

--- src/main/java/pablo/tzeliks/blink_link/domain/user/model/Plan.java
+++ src/main/java/pablo/tzeliks/blink_link/domain/user/model/Plan.java
@@ -1,4 +1,19 @@
 package pablo.tzeliks.blink_link.domain.user.model;
 
+import java.time.LocalDateTime;
+
 public enum Plan {
+    FREE(7),
+    VIP(3650),
+    ENTERPRISE(7300);
+
+    private final int expirationDays;
+
+    Plan(int expirationDays) {
+        this.expirationDays = expirationDays;
+    }
+
+    public LocalDateTime calculateExpiration(LocalDateTime fromDate) {
+        return fromDate.plusDays(this.expirationDays);
+    }
 }
