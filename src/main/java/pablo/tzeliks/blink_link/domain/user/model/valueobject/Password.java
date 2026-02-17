ceb698f1486ead5c125c3054f9ea94222c41ad39
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-17 19:11:34 -0300

    feat: implement Value Object Password

--- src/main/java/pablo/tzeliks/blink_link/domain/user/model/valueobject/Password.java
+++ src/main/java/pablo/tzeliks/blink_link/domain/user/model/valueobject/Password.java
@@ -1,4 +1,10 @@
 package pablo.tzeliks.blink_link.domain.user.model.valueobject;
 
-public class Password {
+public record Password(String value) {
+
+    public Password {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+    }
 }
