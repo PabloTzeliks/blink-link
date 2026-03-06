9a56d1434a15adfee89a3d0d16191e3fe7477832
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-06 19:22:44 -0300

    feat: implement initial classes for Strategy Design Pattern

---
+++ src/main/java/pablo/tzeliks/blink_link/domain/url/strategy/ExpirationCalculationStrategy.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.blink_link.domain.url.strategy;
+
+public interface ExpirationCalculationStrategy {
+}
---
+++ src/main/java/pablo/tzeliks/blink_link/domain/url/strategy/impl/FreePlanStrategy.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.blink_link.domain.url.strategy.impl;
+
+public class FreePlanStrategy {
+}
