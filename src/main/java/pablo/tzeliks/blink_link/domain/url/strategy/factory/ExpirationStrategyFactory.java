0e07e06a513be4ddaa8337613fe61304f8279fbf
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-06 20:35:36 -0300

    feat: implement ExpirationStrategyFactory

--- src/main/java/pablo/tzeliks/blink_link/domain/url/strategy/factory/ExpirationStrategyFactory.java
+++ src/main/java/pablo/tzeliks/blink_link/domain/url/strategy/factory/ExpirationStrategyFactory.java
@@ -1,4 +1,20 @@
 package pablo.tzeliks.blink_link.domain.url.strategy.factory;
 
+import pablo.tzeliks.blink_link.domain.url.strategy.ExpirationCalculationStrategy;
+import pablo.tzeliks.blink_link.domain.url.strategy.impl.EnterprisePlanExpirationStrategy;
+import pablo.tzeliks.blink_link.domain.url.strategy.impl.FreePlanExpirationStrategy;
+import pablo.tzeliks.blink_link.domain.url.strategy.impl.VipPlanExpirationStrategy;
+import pablo.tzeliks.blink_link.domain.user.model.Plan;
+
 public class ExpirationStrategyFactory {
+
+    public static ExpirationCalculationStrategy getExpirationStrategy(Plan userPlan) {
+
+        return switch (userPlan) {
+
+            case FREE -> new FreePlanExpirationStrategy();
+            case VIP -> new VipPlanExpirationStrategy();
+            case ENTERPRISE -> new EnterprisePlanExpirationStrategy();
+        };
+    }
 }
