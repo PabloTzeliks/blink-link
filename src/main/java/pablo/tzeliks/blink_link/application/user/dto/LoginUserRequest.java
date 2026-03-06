15acf15dddc529e6fa9c4e14b99eac316bb360ea
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-06 15:33:53 -0300

    feat: add annotation validation for Login request DTO

--- src/main/java/pablo/tzeliks/blink_link/application/user/dto/LoginUserRequest.java
+++ src/main/java/pablo/tzeliks/blink_link/application/user/dto/LoginUserRequest.java
@@ -1,6 +1,17 @@
 package pablo.tzeliks.blink_link.application.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginUserRequest(
+
+        @JsonProperty("email")
+        @Email(message = "Email must be valid.")
+        @NotBlank(message = "Email must not be blank.")
         String email,
+
+        @JsonProperty("password")
+        @NotBlank(message = "Password must not be blank.")
         String rawPassword
 ) { }
