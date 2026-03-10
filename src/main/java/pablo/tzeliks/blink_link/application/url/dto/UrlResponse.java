96382a930197c2e27bdc8d5e094c666985fca7a7
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-10 16:29:38 -0300

    refactor: implement JsonFormatter for date time on Response Url DTOs

--- src/main/java/pablo/tzeliks/blink_link/application/url/dto/UrlResponse.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/dto/UrlResponse.java
@@ -1,5 +1,6 @@
 package pablo.tzeliks.blink_link.application.url.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
@@ -33,6 +34,10 @@ public record UrlResponse(
         @JsonProperty("original_url") String originalUrl,
         @JsonProperty("short_code") String shortCode,
         @JsonProperty("short_url") String shortUrl,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
         @JsonProperty("created_at") LocalDateTime createdAt,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
         @JsonProperty("expiration_date") LocalDateTime expirationDate
 ) { }
