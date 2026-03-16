0a72a55d8bb00b5fda9a2fa2d39dbd912b3c9516
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-16 14:25:57 -0300

    feat: implement CORS config for allowing cookies on app

--- src/main/java/pablo/tzeliks/blink_link/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/pablo/tzeliks/blink_link/infrastructure/security/config/SecurityConfig.java
@@ -11,9 +11,14 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import pablo.tzeliks.blink_link.infrastructure.security.filter.SecurityFilter;
 import pablo.tzeliks.blink_link.infrastructure.security.oauth2.CustomOAuth2UserService;
 import pablo.tzeliks.blink_link.infrastructure.security.oauth2.OAuth2LoginSuccessHandler;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +51,19 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
