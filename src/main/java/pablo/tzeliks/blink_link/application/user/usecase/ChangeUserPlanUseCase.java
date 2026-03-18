473f103ca4cb4cbc62e2dbe00b67f783d2f561e4
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-17 21:56:27 -0300

    feat: implement initial changing plan for User changing plan

---
+++ src/main/java/pablo/tzeliks/blink_link/application/user/usecase/ChangeUserPlanUseCase.java
@@ -0,0 +1,27 @@
+package pablo.tzeliks.blink_link.application.user.usecase;
+
+import pablo.tzeliks.blink_link.domain.common.exception.DomainException;
+import pablo.tzeliks.blink_link.domain.user.model.Plan;
+import pablo.tzeliks.blink_link.domain.user.model.User;
+import pablo.tzeliks.blink_link.domain.user.ports.UserRepositoryPort;
+
+import java.util.UUID;
+
+public class ChangeUserPlanUseCase {
+
+    private final UserRepositoryPort userRepository;
+
+    public ChangeUserPlanUseCase(UserRepositoryPort userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void execute(UUID userId, Plan newPlan) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        user.changePlan(newPlan);
+
+        userRepository.save(user);
+    }
+}
