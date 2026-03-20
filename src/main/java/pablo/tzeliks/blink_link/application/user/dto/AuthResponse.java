c2a0028a587cac8fca2b7c52248a503a03e6d84d
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 19:46:29 -0300

    refactor: adjust Authentication Response DTO

--- src/main/java/pablo/tzeliks/blink_link/application/user/dto/AuthResponse.java
+++ src/main/java/pablo/tzeliks/blink_link/application/user/dto/AuthResponse.java
@@ -2,25 +2,11 @@ package pablo.tzeliks.blink_link.application.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.UUID;
-
 public record AuthResponse(
 
-        @JsonProperty("id")
-        UUID id,
+        @JsonProperty("user_profile")
+        UserResponse userProfile,
 
-        @JsonProperty("email")
-        String email,
-
-        @JsonProperty("role")
-        String role,
-
-        @JsonProperty("plan")
-        String plan,
-
-        @JsonProperty("createdAt")
-        String createdAt,
-
-        @JsonProperty("updatedAt")
-        String updatedAt
+        @JsonProperty("token")
+        String token
 ) { }
--- src/main/java/pablo/tzeliks/blink_link/application/user/mapper/UserDtoMapper.java
+++ src/main/java/pablo/tzeliks/blink_link/application/user/mapper/UserDtoMapper.java
@@ -18,4 +18,6 @@ public class UserDtoMapper {
                 domain.getUpdatedAt()
         );
     }
+
+    public
 }
