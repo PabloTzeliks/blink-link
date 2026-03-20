aad25cc46ef624584e41c5babb6f31004e9fc0d5
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 19:46:29 -0300

    refactor: adjust Authentication Response DTO

--- src/main/java/pablo/tzeliks/blink_link/application/user/dto/UserResponse.java
+++ src/main/java/pablo/tzeliks/blink_link/application/user/dto/UserResponse.java
@@ -6,14 +6,24 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 public record UserResponse(
-        @JsonProperty("id") String id,
-        @JsonProperty("email") String email,
-        @JsonProperty("role") String role,
-        @JsonProperty("plan") String plan,
+
+        @JsonProperty("id")
+        String id,
+
+        @JsonProperty("email")
+        String email,
+
+        @JsonProperty("role")
+        String role,
+
+        @JsonProperty("plan")
+        String plan,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-        @JsonProperty("created_at") LocalDateTime createdAt,
+        @JsonProperty("created_at")
+        LocalDateTime createdAt,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-        @JsonProperty("updated_at") LocalDateTime updatedAt
+        @JsonProperty("updated_at")
+        LocalDateTime updatedAt
 ) { }
