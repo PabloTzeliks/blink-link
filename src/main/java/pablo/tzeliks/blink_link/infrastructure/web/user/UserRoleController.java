4bdbd287c0b59744d4ff498572cdd9430605fc44
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 20:27:56 -0300

    feat: implement revoking User Admin on Controller

--- src/main/java/pablo/tzeliks/blink_link/infrastructure/web/user/UserRoleController.java
+++ src/main/java/pablo/tzeliks/blink_link/infrastructure/web/user/UserRoleController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pablo.tzeliks.blink_link.application.user.usecase.PromoteUserToAdminUseCase;
+import pablo.tzeliks.blink_link.application.user.usecase.RevokeUserAdminUseCase;
 
 import java.util.UUID;
 
@@ -12,9 +13,13 @@ import java.util.UUID;
 public class UserRoleController {
 
     private final PromoteUserToAdminUseCase promoteUserToAdminUseCase;
+    private final RevokeUserAdminUseCase revokeUserAdminUseCase;
 
-    public UserRoleController(PromoteUserToAdminUseCase promoteUserToAdminUseCase) {
+    public UserRoleController(PromoteUserToAdminUseCase promoteUserToAdminUseCase,
+                              RevokeUserAdminUseCase revokeUserAdminUseCase) {
+
         this.promoteUserToAdminUseCase = promoteUserToAdminUseCase;
+        this.revokeUserAdminUseCase = revokeUserAdminUseCase;
     }
 
     @PatchMapping("/{id}/promote")
@@ -22,6 +27,15 @@ public class UserRoleController {
     public ResponseEntity<Void> promoteToAdmin(@PathVariable UUID id) {
 
         promoteUserToAdminUseCase.execute(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/revoke")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> revokeAdmin(@PathVariable UUID id) {
+
+        revokeUserAdminUseCase.execute(id);
 
         return ResponseEntity.noContent().build();
     }
