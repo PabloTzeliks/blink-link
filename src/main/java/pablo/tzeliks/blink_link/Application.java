77fecd204c9006a770bc77a1fa11fdc304df95ab
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 20:24:20 -0300

    feat: implement Domain method for revoking an Admin role

--- src/main/java/pablo/tzeliks/blink_link/Application.java
+++ src/main/java/pablo/tzeliks/blink_link/Application.java
@@ -58,12 +58,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
