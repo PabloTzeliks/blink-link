ea269aed85b99608d7a6166b096b1c43e6c0e687
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 20:26:34 -0300

    feat: implement Admin revoking use case

---
+++ src/main/java/pablo/tzeliks/blink_link/application/user/usecase/RevokeUserAdminUseCase.java
@@ -0,0 +1,30 @@
+package pablo.tzeliks.blink_link.application.user.usecase;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pablo.tzeliks.blink_link.domain.user.exception.UserNotFoundException;
+import pablo.tzeliks.blink_link.domain.user.model.User;
+import pablo.tzeliks.blink_link.domain.user.ports.UserRepositoryPort;
+
+import java.util.UUID;
+
+@Service
+public class RevokeUserAdminUseCase {
+
+    private final UserRepositoryPort userRepository;
+
+    public RevokeUserAdminUseCase(UserRepositoryPort userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void execute(UUID userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        user.revokeAdmin();
+
+        userRepository.save(user);
+    }
+}
