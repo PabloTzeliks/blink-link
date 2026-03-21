c8fecf319dc186b7be88376fb98e0ba16018ca57
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-21 13:23:00 -0300

    feat: adjust Plan detail for validata a correct Plan

--- src/main/java/pablo/tzeliks/blink_link/domain/user/model/Plan.java
+++ src/main/java/pablo/tzeliks/blink_link/domain/user/model/Plan.java
@@ -1,8 +1,20 @@
 package pablo.tzeliks.blink_link.domain.user.model;
 
+import pablo.tzeliks.blink_link.domain.user.exception.InvalidPlanException;
+
+import java.util.Locale;
+
 public enum Plan {
 
     FREE,
     VIP,
     ENTERPRISE;
+
+    public static Plan fromString(String planName) {
+        try {
+            return Plan.valueOf(planName.toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            throw new InvalidPlanException("Invalid plan.");
+        }
+    }
 }
