d7b32a7b952d467c627b560965971f220d278d30
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-23 16:33:22 -0300

    chore: add swagger docs for CreateUrlRequest DTO

--- src/main/java/pablo/tzeliks/blink_link/application/url/dto/CreateUrlRequest.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/dto/CreateUrlRequest.java
@@ -1,6 +1,7 @@
 package pablo.tzeliks.blink_link.application.url.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
@@ -9,8 +10,11 @@ import org.hibernate.validator.constraints.URL;
  * @version 2.0.0
  * @since 1.0.0
  */
+
+@Schema(description = "Payload to create a new shortened URL")
 public record CreateUrlRequest(
 
+        @Schema(description = "The destination URL to be shortened", example = "https://github.com/PabloTzeliks/blink-link", requiredMode = Schema.RequiredMode.REQUIRED)
         @JsonProperty("original_url")
         @NotBlank(message = "Original URL must not be blank")
         @URL(message = "Original URL must be a valid URL")
