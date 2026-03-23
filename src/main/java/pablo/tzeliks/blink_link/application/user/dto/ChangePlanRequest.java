579b01614c1ce1ca370eaac770991541b62bf742
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-23 16:22:55 -0300

    chore: add swagger docs for ChangePlanRequest DTO

--- src/main/java/pablo/tzeliks/blink_link/application/user/dto/ChangePlanRequest.java
+++ src/main/java/pablo/tzeliks/blink_link/application/user/dto/ChangePlanRequest.java
@@ -1,11 +1,12 @@
 package pablo.tzeliks.blink_link.application.user.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "Payload for changing a user's subscription plan")
 public record ChangePlanRequest(
 
-        @JsonProperty("plan")
-        @NotBlank(message = "Plan must not be blank.")
+        @NotBlank(message = "Plan cannot be blank")
+        @Schema(description = "The new plan to assign to the user (e.g., FREE, PRO, ENTERPRISE)", example = "PRO", requiredMode = Schema.RequiredMode.REQUIRED)
         String plan
-) { }
+) {}
