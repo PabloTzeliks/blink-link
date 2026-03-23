a3aa5d6394bc2204399122223fa3e87eb65486b4
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-23 16:46:36 -0300

    chore: add swagger docs for AuthResponse DTO

--- src/main/java/pablo/tzeliks/blink_link/application/user/dto/AuthResponse.java
+++ src/main/java/pablo/tzeliks/blink_link/application/user/dto/AuthResponse.java
@@ -1,12 +1,16 @@
 package pablo.tzeliks.blink_link.application.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Data transfer object containing the user profile and the authentication token")
 public record AuthResponse(
 
+        @Schema(description = "The profile details of the authenticated user")
         @JsonProperty("user_profile")
         UserResponse userProfile,
 
+        @Schema(description = "JWT authentication token", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjNlN...")
         @JsonProperty("token")
         String token
 ) { }
