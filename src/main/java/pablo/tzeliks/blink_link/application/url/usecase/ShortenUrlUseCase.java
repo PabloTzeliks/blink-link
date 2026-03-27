e64a7dd57e905625272d85ea8575b6c610f74496
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-27 18:33:44 -0300

    feat: implement caching on ShorteUrl use case

--- src/main/java/pablo/tzeliks/blink_link/application/url/usecase/ShortenUrlUseCase.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/usecase/ShortenUrlUseCase.java
@@ -1,12 +1,14 @@
 package pablo.tzeliks.blink_link.application.url.usecase;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pablo.tzeliks.blink_link.application.url.dto.CreateUrlRequest;
 import pablo.tzeliks.blink_link.application.url.dto.UrlDetailsResponse;
 import pablo.tzeliks.blink_link.application.url.mapper.UrlDtoMapper;
-import pablo.tzeliks.blink_link.domain.url.model.Url;
+import pablo.tzeliks.blink_link.application.url.ports.CachePort;
 import pablo.tzeliks.blink_link.application.user.ports.CurrentUserProviderPort;
+import pablo.tzeliks.blink_link.domain.url.model.Url;
 import pablo.tzeliks.blink_link.domain.url.ports.ShortenerPort;
 import pablo.tzeliks.blink_link.domain.url.ports.UrlRepositoryPort;
 import pablo.tzeliks.blink_link.domain.url.strategy.ExpirationCalculationStrategy;
@@ -20,14 +22,23 @@ public class ShortenUrlUseCase {
 
     private final ShortenerPort shortener;
     private final UrlRepositoryPort repository;
-    private final UrlDtoMapper mapper;
     private final CurrentUserProviderPort userProviderPort;
+    private final CachePort cachePort;
+    private final UrlDtoMapper mapper;
 
-    public ShortenUrlUseCase(ShortenerPort shortener, UrlRepositoryPort repository, UrlDtoMapper mapper, CurrentUserProviderPort userProviderPort) {
+    @Value("${app.cache.max-ttl-seconds:604800}")
+    private long maxCacheTtlSeconds;
+
+    public ShortenUrlUseCase(ShortenerPort shortener,
+                             UrlRepositoryPort repository,
+                             CurrentUserProviderPort userProviderPort,
+                             CachePort cachePort, UrlDtoMapper mapper) {
+
         this.shortener = shortener;
         this.repository = repository;
-        this.mapper = mapper;
         this.userProviderPort = userProviderPort;
+        this.cachePort = cachePort;
+        this.mapper = mapper;
     }
 
     @Transactional
@@ -44,6 +55,11 @@ public class ShortenUrlUseCase {
 
         Url url = Url.create(id, userId, request.originalUrl(), shortCode, strategy);
         Url savedUrl = repository.save(url);
+
+        long domainTtl = savedUrl.getSecondsUntilExpiry();
+        long finalCacheTtl = Math.min(domainTtl, maxCacheTtlSeconds);
+
+        cachePort.put(shortCode, savedUrl.getOriginalUrl(), finalCacheTtl);
 
         return mapper.toDto(savedUrl);
     }
