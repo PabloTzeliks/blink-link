c612dbc4351a2a9595e5ce16589f1ee8f1c84162
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-01 16:02:12 -0300

    feat: implement new Application Exception and SequenceAdapter Exception handling

---
+++ src/main/java/pablo/tzeliks/blink_link/application/url/exception/SequenceGenerationException.java
@@ -0,0 +1,7 @@
+package pablo.tzeliks.blink_link.application.url.exception;
+
+public class SequenceGenerationException extends RuntimeException {
+    public SequenceGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
