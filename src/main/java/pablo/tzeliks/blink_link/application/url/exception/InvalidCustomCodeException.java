dae0359c5a050157ce4200bd7d4708758d4e10ef
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-30 20:59:34 -0300

    feat: implement InvalidCustomCodeException

---
+++ src/main/java/pablo/tzeliks/blink_link/application/url/exception/InvalidCustomCodeException.java
@@ -0,0 +1,7 @@
+package pablo.tzeliks.blink_link.application.url.exception;
+
+public class InvalidCustomCodeException extends RuntimeException {
+    public InvalidCustomCodeException(String message) {
+        super(message);
+    }
+}
