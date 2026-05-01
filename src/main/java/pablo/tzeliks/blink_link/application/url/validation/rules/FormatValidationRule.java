149b662c4e1aba42c7cc4046e7795d6b4dae15ed
PabloTzeliks <arq.pabloo@gmail.com>
2026-05-01 19:32:19 -0300

    fix: adjust FormatValidationRule

--- src/main/java/pablo/tzeliks/blink_link/application/url/validation/rules/FormatValidationRule.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/validation/rules/FormatValidationRule.java
@@ -12,13 +12,15 @@ import java.util.regex.Pattern;
 public class FormatValidationRule implements CodeValidationRule {
 
     private static final Pattern VALID_CODE =
-            Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9_-]{2,5}[a-zA-Z0-9]$");
+            Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9_-]{2,18}[a-zA-Z0-9]$");
 
     @Override
     public void validate(String code) {
-        throw new InvalidCustomCodeException(
-                "Custom code must be 4–7 characters, alphanumeric with hyphens or underscores only, " +
-                        "and cannot start or end with a hyphen or underscore."
-        );
+        if (code == null || !VALID_CODE.matcher(code).matches()) {
+            throw new InvalidCustomCodeException(
+                    "Custom code must be 4–7 characters, alphanumeric with hyphens or underscores only, " +
+                            "and cannot start or end with a hyphen or underscore."
+            );
+        }
     }
 }
