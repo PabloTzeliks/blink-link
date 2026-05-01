506344b6ea2c1ab200b4864a45a5ecb333416aa3
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-30 21:55:04 -0300

    feat: implement DuplicateCodeException

---
+++ src/main/java/pablo/tzeliks/blink_link/application/url/exception/DuplicateCodeException.java
@@ -0,0 +1,7 @@
+package pablo.tzeliks.blink_link.application.url.exception;
+
+public class DuplicateCodeException extends RuntimeException {
+    public DuplicateCodeException(String message) {
+        super(message);
+    }
+}
