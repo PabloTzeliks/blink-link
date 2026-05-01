e9063dbcbd5368423f2a3686c91b86635ce681be
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-30 21:55:49 -0300

    feat: implement CodeValidationRule

--- src/main/java/pablo/tzeliks/blink_link/application/url/validation/CodeValidationRule.java
+++ src/main/java/pablo/tzeliks/blink_link/application/url/validation/CodeValidationRule.java
@@ -1,8 +1,6 @@
 package pablo.tzeliks.blink_link.application.url.validation;
 
-import pablo.tzeliks.blink_link.application.url.exception.InvalidCustomCodeException;
-
 public interface CodeValidationRule {
 
-        boolean validate(String code) throws InvalidCustomCodeException;
+        void validate(String code);
 }
